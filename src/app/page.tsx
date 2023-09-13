6a230390161d85a395c9b2643cdc4b7903b8f10d
Giulianno Zanetti <giuzntt@gmail.com>
2023-09-13 10:02:55 -0300

    Curso Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="text-4xl text-center flex items-center gap-5 before:w-0.5 before:h-7 before:bg-sky-500 before:flex">
+        Hello World
+      </h1>
+      <h1 className="text-4xl font-bold ">Hello World Tailwind</h1>
+      <h1 className="text-4xl text-center ">Hello World Tailwind</h1>
+
+      <button
+        className="bg-sky-500 px-4  py-2 rounded font-medium my-2
+        enabled:hover:bg-sky-600 disabled:hover:shadow-lg transition duration-200
+        disabled:opacity-50 disabled:cursor-not-allowed
+      
+      "
+        disabled
+      >
+        Sign in
+      </button>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    // Responsavel por localizar todos os arquivos que contém classes do Tailwind
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        roxo: "#8257e6",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
